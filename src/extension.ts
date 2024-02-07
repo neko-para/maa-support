34f233cdd8629888baf709d58024ac4a51365e74
nekosu <liao.hy@outlook.com>
2024-02-07 22:08:09 +0800

    feat: support partial wpf

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,28 @@
 import * as vscode from 'vscode'
 
 import { PipelineLanguageSupport } from './pipeline'
+import { WpfTaskLanguageSupport } from './wpf/task'
 
 export function activate(context: vscode.ExtensionContext) {
   const pipelineLanguage = new PipelineLanguageSupport()
-  const selector = {
+  const selector: vscode.DocumentSelector = {
     scheme: 'file',
     language: 'json'
+  }
+
+  const wpfTaskLanguage = new WpfTaskLanguageSupport()
+  const wpfTaskSelector: vscode.DocumentSelector = {
+    scheme: 'file',
+    language: 'jsonc',
+    pattern: '**/resource/tasks.json'
   }
 
   context.subscriptions.push(
     vscode.languages.registerDefinitionProvider(selector, pipelineLanguage),
     vscode.languages.registerHoverProvider(selector, pipelineLanguage),
-    vscode.languages.registerCompletionItemProvider(selector, pipelineLanguage, '"', '/')
+    vscode.languages.registerCompletionItemProvider(selector, pipelineLanguage, '"', '/'),
+    vscode.languages.registerDefinitionProvider(wpfTaskSelector, wpfTaskLanguage),
+    vscode.languages.registerHoverProvider(wpfTaskSelector, wpfTaskLanguage)
   )
 }
 
