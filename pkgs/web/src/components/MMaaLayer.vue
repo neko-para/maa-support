4090f0d06d2060c1001b50522f5d3931d21f3987
nekosu <liao.hy@outlook.com>
2024-05-26 13:33:01 +0800

    feat: layer when MaaHttp loss

---
+++ pkgs/web/src/components/MMaaLayer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { NCard } from 'naive-ui'
+
+import { maa } from '@/data/maa'
+</script>
+
+<template>
+  <div
+    class="bg-opacity-50 bg-black absolute z-10 w-full h-full flex flex-col items-center justify-center"
+    v-show="!maa.active"
+  >
+    <n-card class="max-w-96"> MaaHttp not available </n-card>
+  </div>
+</template>
--- pkgs/web/src/view/VMain.vue
+++ pkgs/web/src/view/VMain.vue
@@ -3,9 +3,9 @@ import { NButton, NSplit } from 'naive-ui'
 import { ref } from 'vue'
 
 import MIcon from '@/components/MIcon.vue'
+import MMaaLayer from '@/components/MMaaLayer.vue'
 import { main } from '@/data/main'
-
-import VMainCore from './main/VMainCore.vue'
+import VMainCore from '@/view/main/VMainCore.vue'
 
 const currentId = ref<string | null>(main.active && main.active in main.data ? main.active : null)
 
@@ -25,41 +25,44 @@ function tryDel(id: string) {
 </script>
 
 <template>
-  <n-split :min="0.2" :max="0.4" :default-size="0.2">
-    <template #1>
-      <div class="flex flex-col gap-2 p-4">
-        <div class="flex gap-2">
-          <n-button @click="main.add()">
-            <m-icon> add </m-icon>
-          </n-button>
-        </div>
-        <template v-for="id in main.ids" :key="id">
+  <div class="flex flex-col w-full h-full relative">
+    <m-maa-layer></m-maa-layer>
+    <n-split :min="0.2" :max="0.4" :default-size="0.2">
+      <template #1>
+        <div class="flex flex-col gap-2 p-4">
           <div class="flex gap-2">
-            <n-button
-              @click="
-                () => {
-                  currentId = null
-                  $nextTick(() => {
-                    currentId = id
-                    main.active = id
-                  })
-                }
-              "
-              class="flex-1"
-              :type="currentId === id ? 'primary' : 'default'"
-            >
-              {{ main.data[id].name }}
-            </n-button>
-            <n-button @click="tryDel(id)" class="flex-grow-0">
-              <m-icon> close </m-icon>
+            <n-button @click="main.add()">
+              <m-icon> add </m-icon>
             </n-button>
           </div>
-        </template>
-      </div>
-    </template>
-    <template #2>
-      <v-main-core v-if="currentId" :id="currentId"></v-main-core>
-      <div v-else>select or create instance to continue</div>
-    </template>
-  </n-split>
+          <template v-for="id in main.ids" :key="id">
+            <div class="flex gap-2">
+              <n-button
+                @click="
+                  () => {
+                    currentId = null
+                    $nextTick(() => {
+                      currentId = id
+                      main.active = id
+                    })
+                  }
+                "
+                class="flex-1"
+                :type="currentId === id ? 'primary' : 'default'"
+              >
+                {{ main.data[id].name }}
+              </n-button>
+              <n-button @click="tryDel(id)" class="flex-grow-0">
+                <m-icon> close </m-icon>
+              </n-button>
+            </div>
+          </template>
+        </div>
+      </template>
+      <template #2>
+        <v-main-core v-if="currentId" :id="currentId"></v-main-core>
+        <div v-else>select or create instance to continue</div>
+      </template>
+    </n-split>
+  </div>
 </template>
